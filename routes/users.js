70f21839be51ef1495dd496d9295a222666192b9
Flor <florencia441@hotmail.com>
2017-09-03 09:29:08 -0300

    Gets and posts users

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,90 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+pg.defaults.ssl = true;
 
-// ver que con Postman funciona el get y post
-// hay que pasar la conexión a la db acá y modificar estos métodos
+/**
+ *  Conecta a la base de datos y crea la tabla 'users' de ser necesario.
+ *
+ */
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	const query = client.query('CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY, name VARCHAR(40) not null, surname VARCHAR(40) not null, complete BOOLEAN)');
+  	query.on('end', () => { client.end(); });
+});	
 
-global.users =  [
-	{
-		id:1,
-		name: 'flor',
-		surname: 'rup'
-	}
-];
-
-// returns all the users
+/**
+ *  Devuelve toda la información acerca de todos los users indicados.
+ *
+ */
 router.get('/', function(request, response) {
-	return response.json({
-		users: global.users,
-		error: false
-	});
+  const results = [];
+  // Get a Postgres client from the connection pool
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+
+    // Handle connection errors
+    if (err) {
+      done();
+      console.log(err);
+      return response.status(500).json({success: false, message: "Unexpected error"});
+    }
+
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM users ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return response.json(results);
+    });
+  });
 });
 
-// adds a new user
+/**
+ *  Da de alta un usuario.
+ *
+ */
 router.post('/', function(request, response) {
-	if (!request.body.name) {
-		return response.json({
-			message: 'User name missing',
-			error: true
-		});
-	}
-	global.users.push(request.body);
-	return response.json({
-		message: 'Successfully pushed user',
-		error: false
-	});
+	const results = [];
+  // Grab data from http request
+  const data = {id: request.body.id, name: request.body.name, surname: request.body.surname, complete: false};
+
+  // Get a Postgres client from the connection pool
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    console.log("Connected to db from router.post in users.js");
+
+    // Handle connection errors
+    if (err) {
+      done();
+      console.log(err);
+      return response.status(500).json({success: false, message: "Unexpected error"});
+    }
+
+    // SQL Query > Insert Data
+    client.query('INSERT INTO users(id, name, surname, complete) values($1, $2, $3, $4)',
+    [data.id, data.name, data.surname, data.complete]);
+
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM users ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return response.json(results);
+    });
+  });
 });
+
+
+  
+
 
 // edits an existing user
 router.put('/:userid', function(request, response) {
