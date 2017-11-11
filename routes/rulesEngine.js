a59e146a15ce5cc3e0d8a02ccfead80e7a7f6106
Flor <florencia441@hotmail.com>
2017-11-11 12:25:29 -0300

    Change rules engine

--- routes/rulesEngine.js
+++ routes/rulesEngine.js
@@ -1,52 +1,66 @@
-const Engine = require('json-rules-engine');
-// Set up a new engine
-const ruleEngine = new Engine.Engine();
+const RuleEngine = require('node-rules');
+
 /*istanbul ignore next*/
 
-ruleEngine.addRule({
-	conditions: {
-		any: [{
-			all: [{
-				fact: 'precioPorKmARS',
-				operator: 'equal',
-				value: 15
-			},{
-				fact: 'costoMinimo',
-				operator: 'greaterThanInclusive',
-				value: 50
-			}]
-		}]
-	},
-	event: {  // define the event to fire when the conditions evaluate truthy
-		type: 'costoMinimoSuperado',
-		params: {
-			message: 'El costo supera el mínimo de 50ARS',
-			value: true // Se debe cobrar $COSTO MINIMO porque el saldo es inferior
-		}
-	}
+var rules = [
+/**** Rule 1 ****/
+{
+	"name": "saldoNegativo",
+    "condition": function(R) {
+        R.when(this.saldo < 0);
+    },
+    "consequence": function(R) {
+        this.puedeViajar = false;
+        this.reason = "El saldo del pasajero es negativo";
+        R.stop(); //stop if matched. no need to process next rule.
+    },
+    "priority": 10
+},
+/**** Rule 2 ****/
+{
+	"name": "dominioLlevame",
+    "condition": function(R) {
+        R.when(this.email === "florencia@llevame.com"); // TODO parsear email
+    },
+    "consequence": function(R) {
+        this.puedeViajar = true;
+        this.reason = "El pasajero viaja gratis por tener un email con domino Llevame";
+        R.stop();
+    },
+    "priority": 9
+},
+/**** Last Rule ****/
+{
+	"name": "50ARSMinimo",
+    "condition": function(R) {
+        R.when(this.costoTotal < 50);
+    },
+    "consequence": function(R) {
+        this.puedeViajar = true;
+        this.reason = "El costo minimo es de 50ARS";
+        this.costoTotal = 50;
+        R.stop();
+    },
+    "priority": 1
+}];
+
+/* Creating Rule Engine instance and registering rule */
+var R = new RuleEngine();
+R.register(rules);
+
+var fact = {
+    "saldo": 15,
+    "email": "florencia@gmail.com",
+    "kmRecorridos": 2,
+    "costoTotal": 0
+};
+
+R.execute(fact, function(data) {
+    if (data.puedeViajar) {
+        console.log("Puede viajar con costo: " + data.costoTotal + "\n" + data.reason);
+    } else {
+        console.log("No puede viajar: " + data.reason);
+    }
 });
 
-module.exports = ruleEngine;
-
-/**
- * Define facts the engine will use to evaluate the conditions above.
- * Facts may also be loaded asynchronously at runtime; see the advanced example below
- */
- /*
-let facts = {
-  personalFoulCount: 6,
-  gameDuration: 40
-}
-
-// Run the engine to evaluate
-RulesEngine
-  .run(facts)
-  .then(events => { // run() returns events with truthy conditions
-    events.map(event => console.log(event.params.message))
-});
-*/
-/*
- * Output:
- *
- * Player has fouled out!
- */
+module.exports = R;
