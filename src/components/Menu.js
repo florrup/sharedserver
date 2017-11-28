c703904f7d288b58ea0ae9e4823aaddb6ac26601
florrup <florencia441@hotmail.com>
2017-11-28 08:45:07 -0300

    Hides logout if user is not logged in. Welcomes the user when logged in

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 export default (props) => {
+  var isLoggedIn = (localStorage.getItem('isLoggedIn') == 'true');
+  var link;
+  var linkName;
+  var welcome;
+  if (isLoggedIn) {
+    link = "/logout";
+    linkName = "Logout";
+    welcome = "Bienvenido, " + localStorage.getItem('username');
+  } else {
+    link = "/login";
+    linkName = "Login";
+    welcome = ""
+  }
 	return (
     <nav id="menu">
       <header className="major">
         <h2>Menú</h2>
       </header>
+      <h3>{welcome}</h3>
       <ul>
         <li><a href="/">Homepage</a></li>
         <li>
@@ -20,8 +34,7 @@ export default (props) => {
         <li><a href="/rules">Reglas de Negocio</a></li>
         <li><a href="/trips">Viajes</a></li>
         <br/><br/>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/logout">Logout</a></li>
+        <li><a href={link}>{linkName}</a></li>
       </ul>
     </nav>
 	)
